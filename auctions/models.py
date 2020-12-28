b0a0a9070a3e5801d21f5eb9b6f8f0caadb1bcb0
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2020-12-28 15:00:15 +0700

    Database error

--- auctions/models.py
+++ auctions/models.py
@@ -3,16 +3,6 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
-
-
-class listings(models.Model):
     title = models.CharField(max_length=150)
-
-
-class bids(models.Model):
     bid = models.IntegerField()
-
-
-class comments(models.Model):
     description = models.CharField(max_length=500)
