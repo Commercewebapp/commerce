aa20ed8d5685d3eb389b270fc11dc1b17ee98791
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-01 14:28:04 +0700

    Delete all files

--- auctions/models.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.models import AbstractUser
-from django.db import models
-
-
-class User(AbstractUser):
-    title = models.CharField(max_length=150)
-    bid = models.IntegerField()
-    description = models.CharField(max_length=500)
