34aa2fcbb7a513db1ae3043da68d666b59471ae5
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-02 12:50:25 +0700

    Register models into admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Listing, Bid
+
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "description")
+
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "bid")
+
 
 # Register your models here.
+admin.site.register(Listing, ListingAdmin)
+admin.site.register(Bid, BidAdmin)
