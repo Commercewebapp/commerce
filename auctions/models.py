30548db73a1a103b869c206862ce6b0f296c5056
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-02 12:51:01 +0700

    Create Listing and Bid model

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,12 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Listing(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
+
+class Bid(models.Model):
+    bid = models.IntegerField()
