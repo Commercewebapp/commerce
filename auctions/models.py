d68c7375c22785933fc44d7f57b41ab6322191fc
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-04 08:56:36 +0700

    Create Category model
    Add details to Listing and Bid models

--- auctions/models.py
+++ auctions/models.py
@@ -6,10 +6,28 @@ class User(AbstractUser):
     pass
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
 class Listing(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField(upload_to='listing_images', default=None, null=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
+    starting_price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+
+    def __str__(self):
+        return f"Title: {self.title}, Category: {self.category_id}"
 
 
 class Bid(models.Model):
+    listing_id = models.ForeignKey(Listing, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     bid = models.IntegerField()
+
+    def __str__(self):
+        return f"ID: {self.listing_id}, User: {self.user}, Bid: {self.bid}"
