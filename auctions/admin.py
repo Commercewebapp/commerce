d576c17f213656203388ef563e90c6f9b6ef840c
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-04 08:56:03 +0700

    Create CategoryAdmin class

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 
-from .models import Listing, Bid
+from .models import Listing, Bid, Category
+
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "description")
+    list_display = ("id", "title", "description", "category_id")
 
 
 class BidAdmin(admin.ModelAdmin):
-    list_display = ("id", "bid")
+    list_display = ("id", "listing_id", "bid", "user")
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
 
 
 admin.site.register(Listing, ListingAdmin)
 admin.site.register(Bid, BidAdmin)
+admin.site.register(Category, CategoryAdmin)
