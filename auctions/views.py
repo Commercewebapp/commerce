8464ba2b22d38d7aa4388b088fc784cc50834e3a
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-06 10:22:03 +0700

    Unused HttpResponse

--- auctions/views.py
+++ auctions/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 from django import forms
