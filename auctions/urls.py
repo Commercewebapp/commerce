7dea87a7b88f584309f3a184dec6b148b4274625
Jan "jan" Poonthong <janpoonthong628@gmail.com>
2021-01-08 09:50:02 +0700

    Add url path to listing_id

--- auctions/urls.py
+++ auctions/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("create_listing", views.create_listing, name="create_listing")
+    path("create_listing", views.create_listing, name="create_listing"),
+    path("<int:listing_id>", views.bid, name="bid")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
