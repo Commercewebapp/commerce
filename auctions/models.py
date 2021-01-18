a442bb9eb91fbb008f92e0ee4680ef48f897786f
Jan Poonthong <janpoonthong628@gmail.com>
2021-01-18 07:47:55 +0700

    Create comment model

--- auctions/models.py
+++ auctions/models.py
@@ -2,6 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
+class Comment(models.Model):
+    user = models.ForeignKey("User", on_delete=models.DO_NOTHING)
+    comment = models.TextField()
+    listing_title = models.ForeignKey("Listing", default=None, on_delete=models.DO_NOTHING, related_name="listing_com")
+
+    def __str__(self):
+        return f"{self.user}, Comment: {self.comment}"
+
+
 class Category(models.Model):
     name = models.CharField(max_length=64)
 
@@ -20,11 +29,8 @@ class Listing(models.Model):
     starting_price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
 
     def __str__(self):
-        return f"Title: {self.title}, Category: {self.category_id}"
+        return f"{self.title}"
 
 
 class User(AbstractUser):
     watch_listing = models.ManyToManyField(Listing)
-
-    def __str__(self):
-        return f"User: {self.username}, Watch listing: {self.watch_listing.all()}"
