f093c1052d293fb6b9b90a296fee07aaa7fe8729
Jan Poonthong <janpoonthong628@gmail.com>
2021-01-18 07:47:07 +0700

    Add comment model to admin

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 
-from .models import Listing, Category, User
+from .models import Listing, Category, User, Comment
 
 
 class ListingAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "description", "category_id")
+    list_display = ("id", "title", "description", "category_id", "owner")
 
 
 class BidAdmin(admin.ModelAdmin):
@@ -15,6 +15,11 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
 
 
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "comment", "listing_title")
+
+
 admin.site.register(Listing, ListingAdmin)
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(User)
+admin.site.register(Comment, CommentAdmin)
