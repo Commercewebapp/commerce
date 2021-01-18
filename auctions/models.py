0bfede270a95c60996b8f9052ad0401bc7df2f7c
Jan Poonthong <janpoonthong628@gmail.com>
2021-01-18 11:52:45 +0700

    Add track_user

--- auctions/models.py
+++ auctions/models.py
@@ -5,7 +5,9 @@ from django.db import models
 class Comment(models.Model):
     user = models.ForeignKey("User", on_delete=models.DO_NOTHING)
     comment = models.TextField()
-    listing_title = models.ForeignKey("Listing", default=None, on_delete=models.DO_NOTHING, related_name="listing_com")
+    listing_title = models.ForeignKey("Listing", default=None,
+                                      on_delete=models.DO_NOTHING,
+                                      related_name="listing_com")
 
     def __str__(self):
         return f"{self.user}, Comment: {self.comment}"
@@ -27,6 +29,8 @@ class Listing(models.Model):
     open_at = models.BooleanField(default=True)
     owner = models.ForeignKey("User", null=True, on_delete=models.DO_NOTHING)
     starting_price = models.DecimalField(decimal_places=2, max_digits=10, null=True)
+    track_user = models.ForeignKey("User", null=True, on_delete=models.DO_NOTHING,
+                                   related_name="track")
 
     def __str__(self):
         return f"{self.title}"
