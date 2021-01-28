084c31e1e624cdda3552a7137dc31e9eb4d6ba6d
Jan Poonthong <janpoonthong628@gmail.com>
2021-01-28 09:33:19 +0700

    Fix: models.py deleting connections

--- auctions/models.py
+++ auctions/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 
 class Comment(models.Model):
-    user = models.ForeignKey("User", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
     comment = models.TextField()
     listing = models.ForeignKey("Listing", default=None,
-                                on_delete=models.DO_NOTHING,
+                                on_delete=models.CASCADE,
                                 related_name="comment")
 
     def __str__(self):
@@ -26,8 +26,8 @@ class Listing(models.Model):
     image = models.ImageField(upload_to="listing_images", default=None)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     create_at = models.DateTimeField(auto_now_add=True)
-    open_at = models.BooleanField()
-    owner = models.ForeignKey("User", on_delete=models.DO_NOTHING)
+    open_at = models.BooleanField(null=True, default=True)
+    owner = models.ForeignKey("User", on_delete=models.CASCADE)
     starting_price = models.DecimalField(decimal_places=2, max_digits=8)
 
     def __str__(self):
@@ -40,9 +40,9 @@ class User(AbstractUser):
 
 class Bid(models.Model):
     date = models.DateTimeField(null=True)
-    listing = models.ForeignKey(Listing, on_delete=models.DO_NOTHING,
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE,
                                 related_name="bid")
-    user = models.ForeignKey("User", on_delete=models.DO_NOTHING)
+    user = models.ForeignKey("User", on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.date}"
