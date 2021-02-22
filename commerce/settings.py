c36ef15864a522c654f29e9d4ec1c486003d4562
Jan Poonthong <janpoonthong628@gmail.com>
2021-02-22 15:00:59 +0700

    Revert "Website: deploy to heroku"
    This reverts commit 93fc4f6c34ba5397172d5c0611fb27a6e59fb842.
    I don't want to host the site no more in heroku.

--- commerce/settings.py
+++ commerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,4 +132,4 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
