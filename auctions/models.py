42581d696f21fa6d53a39e84a2fc433c56ccd0a5
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-08 07:51:20 +0700

    Instead of referring to "User" directly, I use settings.AUTH_USER_MODEL

--- auctions/models.py
+++ auctions/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
 from django.db import models
 
 
 class Comment(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     comment = models.TextField()
     listing = models.ForeignKey("Listing", default=None,
                                 on_delete=models.CASCADE,
@@ -28,7 +30,8 @@ class Listing(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     create_at = models.DateTimeField(auto_now_add=True)
     open_at = models.BooleanField(null=True, default=True)
-    owner = models.ForeignKey("User", on_delete=models.CASCADE)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE)
     starting_price = models.DecimalField(decimal_places=2, max_digits=8)
     winning_bid = models.ForeignKey("Bid", on_delete=models.CASCADE,
                                     related_name="won", null=True)
@@ -51,7 +54,8 @@ class Bid(models.Model):
     date = models.DateTimeField(null=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE,
                                 related_name="bids")
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
     bid = models.DecimalField(decimal_places=2, max_digits=8, null=True)
 
     def __str__(self):
@@ -62,7 +66,8 @@ class Flag(models.Model):
     flag_count = models.IntegerField(null=True)
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE,
                                 related_name="flags", null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True,
+                             on_delete=models.CASCADE,
                              related_name="flags_user")
 
     def __str__(self):
