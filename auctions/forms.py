7d072a49486b6db29b728ec0cdc1339b6c305ad7
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-09 12:06:37 +0700

    Add place holder for 'end_date'

--- auctions/forms.py
+++ auctions/forms.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timezone, timedelta
+
 from django import forms
 from django.forms import ModelForm
 
@@ -6,14 +8,18 @@ from .models import Listing, Bid, Comment
 
 class CreateListing(ModelForm):
     class Meta:
+        tomorrow_date = datetime.now(timezone.utc) + timedelta(days=1)
         model = Listing
         fields = ["title", "description", "image", "image_two", "image_three",
-                  "category", "starting_price"]
+                  "category", "starting_price", "end_date"]
         widgets = {
             "title": forms.TextInput(
                 attrs={"class": "form-control col-md-8 col-md-lg-8"}),
             "description": forms.Textarea(
-                attrs={"class": "form-control col-md-8 col-lg-8", "rows": 10})
+                attrs={"class": "form-control col-md-8 col-lg-8", "rows": 10}),
+            "end_date": forms.TextInput(
+                attrs={"placeholder": tomorrow_date.date})
+
         }
 
 
