369d10dfe7fb1d8bccac778d75c8264e0001a307
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-30 16:34:58 +0700

    Add 'white noise' to 'MIDDLEWARE'

--- commerce/settings.py
+++ commerce/settings.py
@@ -51,6 +51,7 @@ INTERNAL_IPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
