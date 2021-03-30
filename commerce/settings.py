367268353448ef1e68bff6e52c13d0fda1721fc7
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-30 11:24:08 +0700

    Open django heroku

--- commerce/settings.py
+++ commerce/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import environ
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,4 +141,4 @@ CRONJOBS = [
 
 LOGIN_URL = "/login"
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
