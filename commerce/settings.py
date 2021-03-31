12a763e0cbd5acac9abb72d90fa53777f9a073fc
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-31 10:48:21 +0700

    Delete left over debug-toolbar

--- commerce/settings.py
+++ commerce/settings.py
@@ -47,7 +47,6 @@ INTERNAL_IPS = [
 
 MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- commerce/urls.py
+++ commerce/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.contrib import admin
 from django.conf import settings
 from django.urls import include, path
@@ -21,5 +20,4 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("auctions.urls")),
-    path("__debug__/", include(debug_toolbar.urls))
 ]
