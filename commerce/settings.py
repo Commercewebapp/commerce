7ee966785ab3c9764674984d43cc3de76b9e4c4e
Jan Poonthong <janpoonthong628@gmail.com>
2021-03-31 10:39:11 +0700

    Turn off 'DEBUG'

--- commerce/settings.py
+++ commerce/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "AHSsdasjdkDJH!@&#*(!@&*(#&!*(#&!*(AJKSDHJKAShdasdjkhasjkdh12@!*&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['commercewebapp.herokuapp.com', '127.0.0.1']
 
