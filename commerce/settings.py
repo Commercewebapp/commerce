2ec02970cee07eb16e7d7c5b35664ab1feaff9d8
jan <janpoonthong628@gmail.com>
2021-04-01 11:44:11 +0700

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -26,7 +26,7 @@ environ.Env.read_env()
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = ['commercewebapp.herokuapp.com', '127.0.0.1']
 
