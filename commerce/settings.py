f2868f10f77b3c9b5e1217b0baf33773e3d0cb58
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-01 15:43:31 +0700

    Config for not uploading images on s3 while on 127.0.0.1

--- commerce/settings.py
+++ commerce/settings.py
@@ -140,12 +140,19 @@ CRONJOBS = [
 
 LOGIN_URL = "/login"
 
-AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+USE_S3 = True
+if USE_S3:
+    AWS_ACCESS_KEY_ID = env('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = env('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
 
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = None
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    DEBUG = False
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+    STATIC_URL = '/static/'
+    DEBUG = True
 
 django_heroku.settings(locals())
