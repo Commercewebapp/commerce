e5c1324cf12430a214af30478d599c6b58480356
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-04 08:20:11 +0700

    Porn detection model

---
+++ auctions/porn_filter.js
@@ -0,0 +1,28 @@
+// Credit: https://github.com/infinitered/nsfwjs#node-js-app
+const axios = require('axios')
+const tf = require('@tensorflow/tfjs-node')
+const fs = require('fs')
+const nsfw = require('nsfwjs')
+async function fn() {
+  const pic = await fs.promises.readFile('/tmp/saved_image.jpg', {
+    responseType: 'arraybuffer',
+  })
+  const model = await nsfw.load()
+  const image = await tf.node.decodeImage(pic,3)
+  const predictions = await model.classify(image)
+  image.dispose()
+
+  function checking() {
+    for (let {className, probability} of predictions) {
+      if (className === "Porn" || className === "Hentai" || className === "Sexy") {
+        if (probability >= 0.5) {
+          return "Not allow";
+        }
+      }
+    }
+    return "Allow";
+  }
+  const result = checking()
+  console.log(result)
+}
+fn()
