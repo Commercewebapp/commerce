792cc7f96f20d3135e074259a15340bcbb51a347
Jan Poonthong <janpoonthong628@gmail.com>
2021-04-06 15:49:57 +0700

    Importing 'get_object_or_404' on a same library

--- auctions/views.py
+++ auctions/views.py
@@ -10,8 +10,7 @@ from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.db import IntegrityError
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views import View
