4ef26580a8c7b094fe481e3730872fd5daa5e015
Jan Poonthong <janpoonthong628@gmail.com>
2021-11-11 09:55:45 +0700

    Add docker-compose

--- commerce/settings.py
+++ commerce/settings.py
@@ -85,6 +85,8 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
